0d2f57522914384974a063fc8763c79384a14245
9ightshade <aniekemeusanga@gmail.com>
2025-01-15 13:39:29 +0100

    chore:routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-
+import Routes from './routes';
+import { AuthProvider } from './hooks/useAuth';
 function App() {
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold text-blue-600 underline">
-        Hello world!
-      </h1>
-    </>
-  )
+  return  <AuthProvider>
+  <Routes />
+</AuthProvider>
 }
 
 export default App

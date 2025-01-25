04402db62c1248f6f17a08d94c75a9fb28e73c60
9ightshade <aniekemeusanga@gmail.com>
2025-01-25 16:32:22 +0100

    chore:update

--- src/admin.ts
+++
@@ -1,8 +0,0 @@
-const adminData = [
-    { id: 1, label: 'Dashboard', color: '#F3F1F1' },
-    { id: 2, label: 'Students', color: '#F3F1F1' },
-    { id: 3, label: 'Transcript Application', color: '#F3F1F1' },
-    { id: 4, label: 'Create Notifications', color: '#F3F1F1' },
-];
-
-export default adminData;
